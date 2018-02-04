1861fa177bcaf3c33fa80b5800ff01cf12f15147
AtomIsHere <ehhmate@gmail.com>
2018-02-04 18:19:45 +1100

    Better format for the name

--- src/main/java/com/github/atomishere/jsonsimple/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * THIS PACKAGE IS NOT BY ME, IT'S AN EDITED VERSION OF JSON-SIMPLE.
- *
- * The original version can be found <a href="https://github.com/fangyidong/json-simple">here</a>.
- */
-package com.github.atomishere.jsonsimple;
